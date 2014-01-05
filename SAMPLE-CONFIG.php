fec94aedd98896e82c8a637a53742fae2df79ba2
Philip Kahn <tigerhawkvok@gmail.com>
2014-01-04 16:56:10 -0800

    Configuration basics

--- SAMPLE-CONFIG.php
+++ SAMPLE-CONFIG.php
@@ -1,3 +1,19 @@
+<?php
+
 /***
  * Update the variables in here and re-save as config.php for use.
  ***/
+
+$default_table='';
+$default_database="";
+$default_user="";
+$default_password="";
+$sql_url = 'localhost';
+
+
+$service_email='';
+$minimum_password_length='15';
+
+$baseurl=""; // define if other than the hosted URL
+
+?>
