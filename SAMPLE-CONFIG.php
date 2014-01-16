db30691dcebba243f61412752c7a542dafef1b51
Philip Kahn <tigerhawkvok@gmail.com>
2014-01-15 17:34:59 -0800

    Handle DB creation

--- SAMPLE-CONFIG.php
+++ SAMPLE-CONFIG.php
@@ -18,4 +18,29 @@ $minimum_password_length='15';
 
 $baseurl=""; // define if other than the hosted URL
 
+$db_cols=array(
+  "username"=>"text",
+  "password"=>"text",
+  "pass_meta"=>"text",
+  "creation"=>"float(16)",
+  "status_tracker"=>"text",
+  "name"=>"text",
+  "flag"=>"boolean",
+  "admin_flag"=>"boolean",
+  "su_flag"=>"boolean"
+  "disabled"=>"boolean"
+  "dtime"=>"int(8)",
+  "last_ip"=>"varchar(32)",
+  "last_login"=>"float(16)",
+  "auth_key"=>"varchar(512)",
+  "data"=>"text",
+  "secdata"=>"text",
+  "special_1"=>"text",
+  "special_2"=>"text",
+  "dblink"=>"varchar(255)",
+  "defaults"=>"text",
+  "public_key"=>"text",
+  "private_key"=>"text"
+);
+
 ?>
