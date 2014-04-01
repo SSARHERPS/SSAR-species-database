873ef9901ce0326cad156af760fbe3505d731fb2
Philip Kahn <tigerhawkvok@gmail.com>
2014-04-01 15:46:27 -0700

    Update common js

--- js/user_common.js
+++ js/user_common.js
@@ -1,36 +1,33 @@
+var goodbg = '#cae682';
+var badbg = '#e5786d';
+// Need global variable to build minimum password length
+
 function checkPasswordLive() {
     // the "8" needs to be passMinLen
-    var goodbg='#cae682';
-    var badbg='#e5786d';
-    var pass=$('#password').val();
-    if(pass.length>=passLengthOverride) {
-        $('#password').css('background',goodbg);
-        basepwgood=true;
+    var pass = $('#password').val();
+    if (pass.length >= passLengthOverride) {
+        $('#password').css('background', goodbg);
+        basepwgood = true;
+    } else if (pass.match(/^(?:(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$)$/)) {
+        $('#password').css('background', goodbg);
+        basepwgood = true;
+    } else {
+        console.log('bad', badbg);
+        $('#password').css('background', badbg);
+        basepwgood = false;
     }
-    else if(pass.match(/^(?:(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$)$/)) {
-        $('#password').css('background',goodbg);
-        basepwgood=true;
-    }
-    else {
-        console.log('bad',badbg);
-        $('#password').css('background',badbg);
-        basepwgood=false;
-    }
-    if(!isNull($('#password2').val())) checkMatchPassword();
+    if (!isNull($('#password2').val())) checkMatchPassword();
     toggleNewUserSubmit();
     return false;
 }
 
 function checkMatchPassword() {
-    var goodbg='#cae682';
-    var badbg='#e5786d';
-    if($('#password').val()==$('#password2').val()) {
-        $('#password2').css('background',goodbg);
-        passmatch=true;
-    }
-    else {
-        $('#password2').css('background',badbg);
-        passmatch=false;
+    if ($('#password').val() == $('#password2').val()) {
+        $('#password2').css('background', goodbg);
+        passmatch = true;
+    } else {
+        $('#password2').css('background', badbg);
+        passmatch = false;
     }
     toggleNewUserSubmit();
     return false;
@@ -38,107 +35,135 @@ function checkMatchPassword() {
 
 function toggleNewUserSubmit() {
     try {
-        var dbool = passmatch && basepwgood ? false:true;
-        $('#createUser_submit').attr('disabled',dbool);
-    }
-    catch(e) {
-        passmatch=false;
-        basepwgood=false;
-    }
-}
-
-
-function isNumber(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-function toFloat(str) {
-    if(isNaN(str) || isEmpty(str) || isBlank(str)) return 0;
-    return parseFloat(str);
-}
-function toInt(str) {
-    if(isNaN(str) || isEmpty(str) || isBlank(str)) return 0;
-    return parseInt(str);
-}
-function isEmpty(str) {
-    return (!str || 0 === str.length);
-}
-function isBlank(str) {
-    return (!str || /^\s*$/.test(str));
-}
-function isNull(str) {
-    try {if(isEmpty(str) || isBlank(str)) return true;}
-    catch(e){return false;}
-    return false;
-}
-function hideShowEl(id) {
-    if($('#'+id).hasClass('hide')) {
-        $('#'+id).hide();
-        $('#'+id).removeClass('hide');   
-    }
-    $('#'+id).toggle('fast');
-}
-function hideShow(containerId) {
-    var container=document.getElementById(containerId);
-    var state=window.getComputedStyle(container).display;
-    if(state=='none') {
-	container.style.display='block';
-    }
-    else {
-	container.style.display='none';
-    }
-}
-jQuery.fn.exists = function(){
-        return jQuery(this).length > 0;
- };
-
-function animateLoad(parentElement,diameter) {
-    if(isNull(diameter)) diameter=50;
-    var elId='#'+parentElement;
-    if($(elId).exists()) {
-	var sm_diameter=roundNumber(diameter*.5,0);
-	$(elId).find('.ball').removeClass('stop nodisp');
-	$(elId).find('.ball1').removeClass('stop nodisp');
-	$(elId).find('.ball').css('width',diameter+"px");
-	$(elId).find('.ball').css('height',diameter+"px");
-	$(elId).find('.ball1').css('width',sm_diameter+"px");
-	$(elId).find('.ball1').css('height',sm_diameter+"px");
-	var offset=roundNumber(diameter/2+sm_diameter/2+9,0);
-	$(elId).find('.ball1').css('top',"-"+offset+"px");
-	return true;
-    }
-    return false;
-}
-
-function stopLoad(parentElement) {
-    var elId='#'+parentElement;
-    if($(elId).exists()) {
-	$(elId).find('.ball').addClass('stop nodisp');
-	$(elId).find('.ball1').addClass('stop nodisp');
+        var dbool = passmatch && basepwgood ? false : true;
+        $('#createUser_submit').attr('disabled', dbool);
+    } catch (e) {
+        passmatch = false;
+        basepwgood = false;
     }
 }
 
-function stopLoadError(parentElement) {
-    var elId='#'+parentElement;
-    if($(elId).exists()) {
-	$(elId).find('.ball1').addClass('bballerror');
-	$(elId).find('.ball').addClass('bballerror');
-	$(elId).find('.ball').addClass('ballerror');
-	$(elId).find('.ball1').addClass('ball1error');
-	setTimeout(function() {
-	    $(elId).find('.ball').addClass('stop nodisp');
-	    $(elId).find('.ball1').addClass('stop nodisp');
-	    $(elId).find('.ball').removeClass('bballerror');
-	    $(elId).find('.ball1').removeClass('bballerror');
-	    $(elId).find('.ball').removeClass('ballerror');
-	    $(elId).find('.ball1').removeClass('ball1error');
-	    },1000);
+if (typeof isNumber !== "function") {
+    window.isNumber = function(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
     }
 }
 
-
-function roundNumber(number, digits) {
-    var multiple = Math.pow(10, digits);
-    var rndedNum = Math.round(number * multiple) / multiple;
-    return rndedNum;
+if (typeof toFloat !== "function") {
+    window.toFloat = function(str) {
+        if (isNaN(str) || isEmpty(str) || isBlank(str)) return 0;
+        return parseFloat(str);
+    }
 }
 
+if (typeof toInt !== "function") {
+    window.toInt = function(str) {
+        if (isNaN(str) || isEmpty(str) || isBlank(str)) return 0;
+        return parseInt(str);
+    }
+}
+
+if (typeof isEmpty !== "function") {
+    window.isEmpty = function(str) {
+        return (!str || 0 === str.length);
+    }
+}
+
+if (typeof isBlank !== "function") {
+    window.isBlank = function(str) {
+        return (!str || /^\s*$/.test(str));
+    }
+}
+
+if (typeof isNull !== "function") {
+    window.isNull = function(str) {
+        try {
+            if (isEmpty(str) || isBlank(str)) return true;
+        } catch (e) {
+            return false;
+        }
+        return false;
+    }
+}
+
+if (typeof hideShowEl !== "function") {
+    window.hideShowEl = function(id) {
+        // legacy
+        id = id.search("#") === 0 ? id : "#" + id;
+        if ($(id).hasClass('hide')) {
+            $(id).hide();
+            $(id).removeClass('hide');
+        }
+        $(id).toggle('fast');
+    }
+}
+
+if (typeof hideShow !== "function") {
+    window.hideShow = function(containerId) {
+        // legacy
+        containerId = containerId.search("#") === 0 ? containerId : "#" + containerId;
+        $(containerId).toggle();
+    }
+}
+
+jQuery.fn.exists = function() {
+    return jQuery(this).length > 0;
+};
+
+if (typeof animateLoad !== "function") {
+    window.animateLoad = function(parentElement, diameter) {
+        if (isNull(diameter)) diameter = 50;
+        var elId = '#' + parentElement;
+        if ($(elId).exists()) {
+            var sm_diameter = roundNumber(diameter * .5, 0);
+            $(elId).find('.ball').removeClass('stop nodisp');
+            $(elId).find('.ball1').removeClass('stop nodisp');
+            $(elId).find('.ball').css('width', diameter + "px");
+            $(elId).find('.ball').css('height', diameter + "px");
+            $(elId).find('.ball1').css('width', sm_diameter + "px");
+            $(elId).find('.ball1').css('height', sm_diameter + "px");
+            var offset = roundNumber(diameter / 2 + sm_diameter / 2 + 9, 0);
+            $(elId).find('.ball1').css('top', "-" + offset + "px");
+            return true;
+        }
+        return false;
+    }
+}
+
+if (typeof stopLoad !== "function") {
+    window.stopLoad = function(parentElement) {
+        var elId = '#' + parentElement;
+        if ($(elId).exists()) {
+            $(elId).find('.ball').addClass('stop nodisp');
+            $(elId).find('.ball1').addClass('stop nodisp');
+        }
+    }
+}
+
+if (typeof stopLoadError !== "function") {
+    window.stopLoadError = function(parentElement) {
+        var elId = '#' + parentElement;
+        if ($(elId).exists()) {
+            $(elId).find('.ball1').addClass('bballerror');
+            $(elId).find('.ball').addClass('bballerror');
+            $(elId).find('.ball').addClass('ballerror');
+            $(elId).find('.ball1').addClass('ball1error');
+            setTimeout(function() {
+                $(elId).find('.ball').addClass('stop nodisp');
+                $(elId).find('.ball1').addClass('stop nodisp');
+                $(elId).find('.ball').removeClass('bballerror');
+                $(elId).find('.ball1').removeClass('bballerror');
+                $(elId).find('.ball').removeClass('ballerror');
+                $(elId).find('.ball1').removeClass('ball1error');
+            }, 1000);
+        }
+    }
+}
+
+if (typeof roundNumber !== "function") {
+    window.roundNumber = function(number, digits) {
+        var multiple = Math.pow(10, digits);
+        var rndedNum = Math.round(number * multiple) / multiple;
+        return rndedNum;
+    }
+}
