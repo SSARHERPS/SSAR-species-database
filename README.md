3a993ae2714f3f0b6bfea4470cad0c066eb4cacf
Philip Kahn <tigerhawkvok@gmail.com>
2014-08-09 17:30:44 -0700

    Core loop of search

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ image # hit calphotos api if this field is empty
 ### Default search
 
 1. Case 1: No space:
-  Lookup columns `common_name`,`genus`,`species`,`minor_type`, and `deprecated_scientific` in that order. Filter types if specified.
+  Lookup columns `common_name`,`genus`,`species`,`major_common_type`,`major_subtype`, and `deprecated_scientific` in that order. Filter types if specified.
 2. Case 2: 1 or 2 spaces:
   Lookup matched pair for exploded string in `genus`,`species`,`subspecies`; if this returns a hit, stop. Otherwise, look at whole string in `common name`
 3. Case 3: Numeric:
@@ -61,9 +61,10 @@ image # hit calphotos api if this field is empty
 
 ### Search flags
 
-1. `fuzzy`: if truthy, use a similar sounding search for results, like SOUNDEX.
+1. `fuzzy`: if truthy, use a similar sounding search for results, like SOUNDEX. Note this won't work for authority years or deprecated scientific names.
 2. `only`: restrict search to this csv column list. Return an error if invalid column specified.
 3. `include`: Include the additional columns in this csv list. Return an error if invalid column specified.
 4. `type`: restrict search to this `major_type`. Literal scientific match only. Return an error if the type does not exist.
 5. `filter`: restrict search by this list of {"`column`":"value"} object list. Requires key "BOOLEAN_TYPE" set to either "AND" or "OR". Return an error if the key does not exist, or if an unknown column is specified.
 6. `limit`: Search result return limit.
+7. `loose`: Don't check for strict matches, allow partials
--- commonnames_api.php
+++ commonnames_api.php
@@ -46,10 +46,10 @@ if(!function_exists('elapsed'))
 
       if(!is_numeric($start_time))
         {
-          global $start_script_timer;          
+          global $start_script_timer;
           if(is_numeric($start_script_timer)) $start_time = $start_script_timer;
           else return false;
-        }      
+        }
       return 1000*(microtime_float() - (float)$start_time);
     }
   }
@@ -82,32 +82,49 @@ function checkColumnExists($column)
  *****************************/
 
 $flag_fuzzy = boolstr($_REQUEST['fuzzy']);
+$loose = boolstr($_REQUEST['loose']);
 # Default limit is specified in CONFIG
 $limit = is_numeric($_REQUEST['limit']) && $_REQUEST['limit'] >= 1 ? intval($_REQUEST['limit']):$default_limit;
 
 checkColumnExists($_REQUEST['only']);
 checkColumnExists($_REQUEST['include']);
 
+$params = array();
+$boolean_type = false;
 if(isset($_REQUEST['filter']))
   {
     $params = smart_decode64($_REQUEST['filter']);
-    if($params === false)
+    if(empty($params))
       {
-        $params = json_decode($_REQUEST['filter'],true);
+        $params_temp = json_decode($_REQUEST['filter'],true);
+        if(!empty($params_temp))
+          {
+            $params = array();
+            foreach($params_temp as $col=>$lookup)
+              {
+                $params[$db->sanitize(deEscape($col))] = $db->sanitize(deEscape($lookup));
+              }
+          }
       }
-    # Does the "BOOLEAN_TYPE" key exist?
-    if(!array_key_exists("BOOLEAN_TYPE",$params)) returnAjax(array("status"=>false,"error"=>"Missing required parameter","human_error"=>"The key 'BOOLEAN_TYPE' must exist and be either 'AND' or 'OR'.","given"=>$params));
-    # Is it valid?
-    if($params['BOOLEAN_TYPE'] != "AND" || $params['BOOLEAN_TYPE'] != "OR") returnAjax(array("status"=>false,"error"=>"Missing required parameter","human_error"=>"The key 'BOOLEAN_TYPE' must exist and be either 'AND' or 'OR'.","given"=>$params));
-    # Do all the columns exist?
-    foreach($params as $col=>$lookup)
+    if(!empty($params))
       {
-        checkColumnExists($col);
+        # Does the "BOOLEAN_TYPE" key exist?
+        if(!array_key_exists("BOOLEAN_TYPE",$params)) returnAjax(array("status"=>false,"error"=>"Missing required parameter","human_error"=>"The key 'BOOLEAN_TYPE' must exist and be either 'AND' or 'OR'.","given"=>$params));
+        # Is it valid?
+        if($params['BOOLEAN_TYPE'] != "AND" || $params['BOOLEAN_TYPE'] != "OR") returnAjax(array("status"=>false,"error"=>"Missing required parameter","human_error"=>"The key 'BOOLEAN_TYPE' must exist and be either 'AND' or 'OR'.","given"=>$params));
+        $boolean_type = $params['BOOLEAN_TYPE'];
+        unset($params['BOOLEAN_TYPE']);
+        # Do all the columns exist?
+        foreach($params as $col=>$lookup)
+          {
+            checkColumnExists($col);
+          }
       }
-    
   }
 
+
 $search = $db->sanitize(deEscape($_REQUEST['q']));
+
 
 
 /*****************************
@@ -135,6 +152,142 @@ function areSimilar($string1,$string2,$distance=70,$depth=3)
   return false;
 }
 
-# Parse out the parameters before doing the query
+function handleParamSearch($filter_params,$extra_params = false)
+{
+  /***
+   * Handle the searches when they're using advanced options
+   *
+   * @return array the result vector
+   ***/
+  return false;
+}
+
+/****
+ * The actual main search loop
+ ****/
+
+$result_vector = array();
+if(empty($params) || !empty($search))
+  {
+    # There was either a parsing failure, or no filter set.
+    if(empty($search)) $search = "*"; # Wildcard it. Be greedy.
+    if(is_numeric($search))
+      {
+        $method="year_search";
+        $col = "authority_year";
+        $loose = true; # Always true because of the way data is stored
+        $params[$col] = $search;
+        $r = $db->doQuery($params,"*",$boolean_type,$loose,true);
+        #######TODO HANDLE ERROR
+        while($row = mysqli_fetch_assoc($r))
+          {
+            $result_vector[] = $row;
+          }
+      }
+    else if (strpos(" ",$search) === false)
+      {
+        $method="spaceless_search";
+        # No space in search
+        if($boolean_type !== false)
+          {
+            # Handle the complicated statement. It'll need to be
+            # escaped from normal handling.
+            $result_vector = handleParamSearch();
+          }
+        else
+          {
+            $boolean_type = "OR";
+            $params["common_name"] = $search;
+            $params["genus"] = $search;
+            $params["species"] = $search;
+            $params["major_common_type"] = $search;
+            $params["major_subtype"] = $search;
+            $params["deprecated_scientific"] = $search;
+            if(!$flag_fuzzy)
+              {
+                $r = $db->doQuery($params,"*",$boolean_type,$loose,true);
+                #######TODO HANDLE ERROR
+                while($row = mysqli_fetch_assoc($r))
+                  {
+                    $result_vector[] = $row;
+                  }
+              }
+            else
+              {
+                foreach($params as $search_column=>$search_criteria)
+                  {
+                    $r = $db->doSoundex(array($search_column=>$search_criteria),"*",true);
+                    ############ TODO HANDLE ERRORS
+                    while($row = mysqli_fetch_assoc($r))
+                      {
+                        $result_vector[] = $row;
+                      }
+                  }
+              }
+          }
+      }
+    else
+      {
+        # Spaces in search
+        if($boolean_type !== false)
+          {
+            # Handle the complicated statement. It'll need to be
+            # escaped from normal handling.
+            $result_vector = handleParamSearch();
+          }
+        else
+          {
+            $exp = explode(" ",$search);
+            $fallback = true;
+            if(sizeof($exp) == 2 || sizeof($exp) == 3)
+              {
+                $boolean_type = "and";
+                $params["genus"] = $exp[0];
+                $params["species"] = $exp[1];
+                if(sizeof($exp) == 3) $params["subspecies"] = $exp[2];
+                $r = $db->doQuery($params,"*",$boolean_type,$loose,true);
+                #######TODO HANDLE ERROR
+                if(mysqli_num_rows($r) > 0)
+                  {
+                    while($row = mysqli_fetch_assoc($r))
+                      {
+                        $result_vector[] = $row;
+                      }
+                    $method = "scientific";
+                    $fallback = false;
+                  }
+                else
+                  {
+                    # Always has to be a loose query
+                    $r = $db->doQuery(array("deprecated_scientific"=>$search),"*",$boolean_type,true,true);
+                    ###### TODO HANDLE ERRORS
+                    while($row = mysqli_fetch_assoc($r))
+                      {
+                        $result_vector[] = $row;
+                      }
+                    $method = "deprecated_scientific";
+                    $fallback = false;
+                  }
+              }
+            if($fallback)
+              {
+                $method = "space_fallback";
+                $params["common_name"] = $search;
+                $r = $db->doQuery($params,"*",$boolean_type,$loose,true);
+                while($row = mysqli_fetch_assoc($r))
+                  {
+                    $result_vector[] = $row;
+                  }
+              }
+          }
+      }
+  }
+else
+  {
+    $result_vector = handleParamSearch();
+  }
+
+returnAjax(array("status"=>true,"result"=>$result_vector,"count"=>sizeof($result_vector),"method"=>$method));
+
 
 ?>
