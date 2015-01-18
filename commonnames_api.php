c373192decf576bfa21ac06a1c8c3734e8f3e747
Philip Kahn <tigerhawkvok@gmail.com>
2015-01-17 20:46:21 -0800

    Fix potential bad parse

--- commonnames_api.php
+++ commonnames_api.php
@@ -56,7 +56,8 @@ function returnAjax($data)
   header('Cache-Control: no-cache, must-revalidate');
   header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
   header('Content-type: application/json');
-  print json_encode($data,JSON_FORCE_OBJECT);
+  $json = json_encode($data,JSON_FORCE_OBJECT);
+  print str_replace("&#34;","\"",$json);
   exit();
 }
 
